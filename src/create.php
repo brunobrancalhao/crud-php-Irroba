1e65eae3476851ea690f920b1759d9790bf5e833
Bruno Brancalhao <bruno@irroba.com.br>
2020-09-17 17:53:16 -0300

    init create

--- src/create.php
+++ src/create.php
@@ -1,6 +1,24 @@
 <?php
 
 require_once "connection.php";
+if(isset($_POST['save']))
+{
+    $name = $_POST['name'];
+    $mobile = $_POST['mobile'];
+    $email  = $_POST['email'];
+    $sql = "INSERT INTO users (name,mobile,email)
+    VALUES('$name', '$mobile', '$email')";
+
+    if(mysqli_query($conn, $sql)) {
+        header("location: index.php");
+        exit();
+    } else {
+        echo "Error: " . $sql . " " . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+}
+
 
 ?>
 
